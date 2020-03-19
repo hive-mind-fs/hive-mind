be37350c1f84cbf9da24299f338d8c3f685d0a86
Natasha Whitney <natiwhitney@gmail.com>
2020-03-19 17:29:53 -0400

    Routes import lowercased files

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,13 @@
-const router = require('express').Router()
-module.exports = router
+const router = require("express").Router();
+module.exports = router;
 
-router.use('/Users', require('./Users'))
-router.use('/Games', require('./products'))
-router.use('/Rounds', require('./Rounds'))
-router.use('/Words', require('./Words'))
+router.use("/user", require("./user"));
+router.use("/game", require("./game"));
+router.use("/round", require("./round"));
+router.use("/word", require("./word"));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
