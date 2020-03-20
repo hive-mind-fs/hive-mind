207b557a462ade3e0fb0605eff6b770d71f64e06
Bobby Nixon <rhanixon1@gmail.com>
2020-03-20 15:08:15 -0700

    shuffle letters button now works

--- client/screens/GameBoardScreen.js
+++ client/screens/GameBoardScreen.js
@@ -1,63 +1,57 @@
 import React, { Component } from 'react';
-import { Button, Text, StyleSheet, View } from 'react-native';
-import { Body, Card, CardItem, Container, Left, Right } from 'native-base';
+import { Button, StyleSheet, View } from 'react-native';
+import { Container } from 'native-base';
 import Hive from '../components/Hive';
 import Input from '../components/Input';
 
 // const wordsGot = ['this', 'team', 'is', 'poppin'];
-// const cl = 'A'
-// const letters = ['B', 'C', 'D', 'E', 'F', 'G'];
-// const input = [];
 
 //shuffling algorithm: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
-const shuffle = (arr) => {
+const shuffle = arr => {
   for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
 };
 
-export default class GameBoardScreen extends Component{
-
+export default class GameBoardScreen extends Component {
   state = {
     input: [],
     cl: 'A',
     letters: ['B', 'C', 'D', 'E', 'F', 'G']
-    };
+  };
 
-render() {
-  return (
-    <Container
-      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-    >
-    <Input inputLetters={this.state.input} />
-      <Hive
-        centerLetter={this.state.cl}
-        otherLetters={this.state.letters}
-        onLetterPress={letter => {
-          let input = this.state.input;
-          input.push(letter);
-          this.setState(input);
-        }}
-      />
-      <View style={styles.flexRow}>
-        <Button
-          title="Delete"
-          onPress={() => input.pop()}
+  render() {
+    return (
+      <Container
+        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      >
+        <Input inputLetters={this.state.input} />
+        <Hive
+          centerLetter={this.state.cl}
+          otherLetters={this.state.letters}
+          onLetterPress={letter => {
+            let input = this.state.input;
+            input.push(letter);
+            this.setState(input);
+          }}
         />
-        <Button
-          title="Shuffle"
-          onPress={() => shuffle(letters)}
-        />
-        <Button
-          title="Enter"
-          onPress={() => navigation.navigate('DashboardScreen')}
-        />
-      </View>
-    </Container>
-  );
-}
+        <View style={styles.flexRow}>
+          <Button title="Delete" onPress={() => input.pop()} />
+          <Button title="Shuffle" onPress={() => {
+            let letters = this.state.letters;
+            letters = shuffle(letters);
+            this.setState(letters);
+            }} />
+          <Button
+            title="Enter"
+            onPress={() => navigation.navigate('DashboardScreen')}
+          />
+        </View>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
