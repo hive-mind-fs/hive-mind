2c3643b3a21c3d10d63dfc624c63c0434ff21e79
Bobby Nixon <rhanixon1@gmail.com>
2020-03-20 10:52:56 -0700

    all associations seeded except UserRoundWords

--- script/seed.js
+++ script/seed.js
@@ -2,7 +2,7 @@
 
 const db = require('../server/db');
 
-const { User, Round, Game } = require('../server/db/models');
+const { User, Round, Game, Word } = require('../server/db/models');
 
 const dummyUsers = require('../server/db/dummyData/dummyUsers.js');
 const dummyRounds = require('../server/db/dummyData/dummyRounds.js');
@@ -14,15 +14,40 @@ async function seed() {
   await User.bulkCreate(dummyUsers);
   await Round.bulkCreate(dummyRounds);
   await Game.bulkCreate(dummyGames);
-  console.log(`seeded successfully`);
 
   //seed associations
   try {
     for (let i = 1; i <= 50; i++) {
+      //A game can have many rounds
       let game = await Game.findByPk(i);
-        let round = await Round.findByPk(i);
-        await game.addRound(round);
+      let round = await Round.findByPk(i);
+      await game.addRound(round);
+      //Each game has a winner
+      let user = await User.findByPk(Math.ceil(Math.random() * 50));
+      await game.setWinner(user);
+      //Each round has a winner
+      await round.setWinner(user);
     }
+
+    //Seed the roundWords thru table
+    const round1 = await Round.findByPk(1)
+    await round1.addWords([
+      await Word.create({ word: "WORDONE" }),
+      await Word.create({ word: "WORDTWO" }),
+      await Word.create({ word: "WORDTHREE" }),
+      await Word.create({ word: "WORDFOUR" })
+    ]);
+
+    //Seed userRounds thru table
+    const round2 = await Round.findByPk(2);
+    const user1 = await User.findByPk(1);
+    const user2 = await User.findByPk(2);
+    await round2.addUsers([user1, user2]);
+
+
+
+    console.log(`seeded successfully`);
+
   } catch (error) {
     console.log(error);
   }
