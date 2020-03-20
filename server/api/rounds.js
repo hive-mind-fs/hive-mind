8aa242f1cb83a573576b1cd3317d3f7e43f5a115
Natasha Whitney <natiwhitney@gmail.com>
2020-03-20 15:43:20 -0400

    Flesh out rounds api

--- server/api/rounds.js
+++ server/api/rounds.js
@@ -1,14 +1,48 @@
 const router = require("express").Router();
-const { Round } = require("../db/models");
+const { Round, Word, User } = require("../db/models");
 const { isAdmin, isCorrectUser, isSession } = require("./gateway");
 
 module.exports = router;
 
+// one route returns round with everything that we need
+// words (possible words for round)
+// user round (we think)
+/// guessed words
+// to do: limit attributes
+// to do: get ONE random round
+// to do: add user when we grab the round
+
+// userRounds --
+// userRoundWords --
+router.get("/:id", async (req, res, next) => {
+  try {
+    const rounds = req.params.id;
+    await Round.findByPk(+req.params.id, {
+      include: [{ model: Word }]
+    });
+    res.json(rounds);
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.get("/", async (req, res, next) => {
   try {
-    // include words
-    const rounds = await Round.findAll();
+    const rounds = await Round.findAll({
+      include: [{ model: Word }]
+    });
     res.json(rounds);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// post round info to db
+// create a new entry in user rounds on POST
+router.post("/", async (req, res, next) => {
+  try {
+    const round = req.body;
+    console.log("round was passed", round);
   } catch (err) {
     next(err);
   }
