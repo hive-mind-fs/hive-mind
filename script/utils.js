8e28121aa2a992626753b471ca2ba45befd9de3b
Natasha Whitney <natiwhitney@gmail.com>
2020-03-20 09:30:02 -0400

    Dict work in progress

---
+++ script/utils.js
@@ -0,0 +1,9 @@
+const { Round, Word } = require("../server/db/models");
+
+// deserialize hash map
+
+const lettersToWords = async letters => {
+  return letters;
+};
+
+module.exports = lettersToWords;
