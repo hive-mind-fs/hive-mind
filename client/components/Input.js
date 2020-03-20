193661eb61e25326745ae97cbf3e1063c4f68adc
Bobby Nixon <rhanixon1@gmail.com>
2020-03-20 16:05:45 -0700

    code formatting

--- client/components/Input.js
+++ client/components/Input.js
@@ -1,20 +1,20 @@
-import { View, Text} from 'native-base';
+import { View, Text } from 'native-base';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-letter: {
-  position: 'absolute',
-  textAlign: 'center',
-  fontSize: 40,
-  fontWeight: 'bold'
-}
+  letter: {
+    position: 'absolute',
+    textAlign: 'center',
+    fontSize: 40,
+    fontWeight: 'bold'
+  }
 });
 
 const Input = ({ inputLetters }) => {
   return (
     <View style={{ width: 300, height: 100 }}>
-   <Text style={styles.letter}>{inputLetters}</Text>
+      <Text style={styles.letter}>{inputLetters}</Text>
     </View>
   );
 };
