890f60c8325d67e13b9850285873e2e5246379a4
Natasha Whitney <natiwhitney@gmail.com>
2020-03-22 16:37:31 -0400

    User rounds

--- server/api/userRounds.js
+++ server/api/userRounds.js
@@ -1,20 +1,35 @@
 const router = require('express').Router();
 const { Round, Word, User, UserRound, GuessedWord } = require('../db/models');
 const { isAdmin, isCorrectUser, isSession } = require('./gateway');
+const {USERROUND_ATTRIBUTES, ROUND_ATTRIBUTES, WORD_ATTRIBUTES} = require('./utils')
 
 module.exports = router;
 
 // Create
 // Given user id, create new user rounds entry with random round
 router.post('/:userId', async (req, res, next) => {
-  try {
-    const userId = +req.params.userId;
-    const round = await Round.getRandom()
-    const userRound = await UserRound.create({userId: userId, roundId: round.id})
-    res.send(userRound)
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const userId = +req.params.userId;
+        //const round = await Round.getRandom()
+        // To do: programatically generate words for round
+        const round = await Round.findByPk(51)
+        const userRound = await UserRound.create({ userId: userId, roundId: round.id })
+        const userRoundWithAssociations = await UserRound.findByPk(userRound.id, {
+            attributes: USERROUND_ATTRIBUTES,
+            include: [
+                { model: Word, attributes: WORD_ATTRIBUTES },
+                {
+                    model: Round,
+                    attributes: ROUND_ATTRIBUTES,
+                    include : [{model: Word}]
+                },
+            ]
+        })
+        res.send(userRoundWithAssociations)
+    } catch (err) {
+        next(err);
+    }
 });
 
-// Given user id, round is retrieve ...
+// Update
+// Given userRound id, persist to database
