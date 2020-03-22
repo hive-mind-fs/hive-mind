c2ef06dcaa7e27b7086fdf666cb53bb6c0f474bb
Natasha Whitney <natiwhitney@gmail.com>
2020-03-22 16:59:41 -0400

    Create userRound store

--- client/screens/PlayScreen.js
+++ client/screens/PlayScreen.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { connect } from 'redux';
 import { Button, Text } from 'react-native';
 import { Container, H1 } from 'native-base';
 import { Logo } from '../components';
+import { fetchPracticeRound } from '../store'
 
-export default function PlayScreen({ navigation }) {
+export default function PlayScreen({ navigation, createUserRound }) {
   return (
-    <Container style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Logo/>
-        <H1>Spelling Bee</H1>
-        <Text>How many words can you make with 7 letters?</Text>
+    <Container
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+    >
+      <Logo />
+      <H1>Spelling Bee</H1>
+      <Text>How many words can you make with 7 letters?</Text>
       <Button
         title="Start Playing"
-        onPress={() => navigation.navigate('CountdownScreen')}
+        onPress={() => {
+          createUserRound(10) // To do, replace with user id
+          navigation.navigate('CountdownScreen')
+        }
       />
       <Button
         title="Rules"
@@ -20,3 +27,9 @@ export default function PlayScreen({ navigation }) {
     </Container>
   );
 }
+
+const mapDispatch = dispatch => {
+  return {
+    createUserRound: (userId) => dispatch(fetchPracticeRound(userId))
+  };
+};
--- client/store/game.js
+++ client/store/game.js
@@ -6,53 +6,65 @@ import axios from 'axios';
  * INITIAL STATE
  */
 const defaultGame = {
-    gameStatus: 'countdown',
-    round: {}
+  gameStatus: 'countdown',
+  round: {}
 };
-
 
 /**
  * ACTION TYPES
  */
 const SET_GAME_STATUS = 'SET_GAME_STATUS';
 const START_ROUND = 'GET_ROUND';
+const SET_PRACTICE_ROUND = 'SET_PRACTICE_ROUND';
 
 /**
  * ACTION CREATORS
  */
 const setGameStatus = gameStatus => ({ type: SET_GAME_STATUS, gameStatus });
 
-const startRound = round => ({ type: START_ROUND, round })
+const startRound = round => ({ type: START_ROUND, round });
+
+const setPracticeRound = practiceRound => ({
+  type: SET_PRACTICE_ROUND,
+  user_round
+});
 
 /**
  * THUNK CREATORS
  */
 export const setGameStatusThunk = gameStatus => async dispatch => {
-    try {
-        dispatch(setGameStatus);
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    dispatch(setGameStatus);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const startRoundThunk = () => async dispatch => {
   try {
-      dispatch(startRound);
+    dispatch(startRound);
   } catch (err) {
-      console.error(err);
+    console.error(err);
   }
 };
 
+export const fetchPracticeRound = userId => async dispatch => {
+  try {
+    const practiceRound = await axios.post(`/api/userRound/{userId}`);
+    dispatch(setPracticeRound(practiceRound));
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 /**
  * REDUCER
  */
 export default function(state = defaultGame, action) {
-    switch (action.type) {
-      case SET_GAME_STATUS:
-        return {...state}, action.gameStatus;
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case SET_GAME_STATUS:
+      return { ...state }, action.gameStatus;
+    default:
+      return state;
   }
-  
+}
