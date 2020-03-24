388608ca9844f1a53a8808d0917cf8bbdab95010
Vosters <mike.r.vosters@gmail.com>
2020-03-24 17:37:00 -0600

    Added basic How To Play Instructions to RulesScreen

--- client/screens/GameBoardScreen.js
+++ client/screens/GameBoardScreen.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
-=======
-import React, { useState } from 'react';
->>>>>>> 1cb855c1d926c6bcc757bb5af9122a2d65dc8792
 import { connect } from 'react-redux';
 import { Button, StyleSheet, View, Text } from 'react-native';
 import { Container } from 'native-base';
