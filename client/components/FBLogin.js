b8cffc0820dd0c14979e9e1114550725d1e237bc
Bobby Nixon <rhanixon1@gmail.com>
2020-03-25 11:39:05 -0700

    when a user logs in with facebook there name and profile picture are loaded

--- client/components/FBLogin.js
+++ client/components/FBLogin.js
@@ -1,33 +1,50 @@
-import React, { Component } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { Text, TouchableOpacity, View, Image } from 'react-native';
 // import Expo  from 'expo';
 import * as Facebook from 'expo-facebook';
 
-export default class FBLogin extends Component {
-  state = {
-    responseJSON: null,
-  };
-  callGraph = async token => {
-    /// Look at the fields... I don't have an `about` on my profile but everything else should get returned.
-    const response = await fetch(
-      `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,about,picture`
-    );
-    const responseJSON = JSON.stringify(await response.json());
-    this.setState({ responseJSON });
-  };
+export default function FBLogin(){
 
-  login = async () => {
+const [isLoggedin, setLoggedinStatus] = useState(false);
+const [userData, setUserData] = useState([]);
+//   callGraph = async token => {
+//     /// Look at the fields... I don't have an `about` on my profile but everything else should get returned.
+//     const response = await fetch(
+//       `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,about,picture`
+//     );
+//     const userData = JSON.stringify(await response.json());
+//     setUserData({ responseJSON });
+//   };
+
+  const login = async () => {
+    try {
     await Facebook.initializeAsync('3845977412082426');
     const {type, token } = await Facebook.logInWithReadPermissionsAsync({permissions: ['public_profile', 'email', 'user_friends'],
     });
 
     if (type === 'success') {
-      this.callGraph(token);
+        fetch(`https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,picture.height(500)`)
+        .then(response => response.json())
+        .then(data => {
+          setLoggedinStatus(true);
+          setUserData(data);
+        })
+        .catch(e => console.log(e));
+     } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
     }
-  };
+};
 
-  renderButton = () => (
-    <TouchableOpacity onPress={() => this.login()}>
+const logout = () => {
+    setLoggedinStatus(false);
+    setUserData(null);
+  }
+
+  const renderButton = () => (
+    <TouchableOpacity onPress={() => login()}>
       <View
         style={{
           width: '100%',
@@ -42,19 +59,31 @@ export default class FBLogin extends Component {
       </View>
     </TouchableOpacity>
   );
-  renderValue = value => (
+
+    const  renderValue = value => (
     <Text key={value} >{value}</Text>
   );
-  render() {
-    return (
-      <View >
-        {this.state.responseJSON &&
-          this.renderValue('User data : ' + this.state.responseJSON)}
 
-        {this.renderButton()}
+    return (
+    isLoggedin ?
+        userData ?
+      <View >
+       <Image
+            style={{ width: 200, height: 200, borderRadius: 50 }}
+            source={{ uri: userData.picture.data.url }}
+            onLoadEnd={() => setImageLoadStatus(true)} />
+    <Text style={{ fontSize: 22, marginVertical: 10, alignContent: "center"}}>Hi {userData.name}!</Text>
+
+
       </View>
+      :
+      null
+      :
+      <View >
+      {renderButton()}
+    </View>
     );
   }
-}
+
 
 
