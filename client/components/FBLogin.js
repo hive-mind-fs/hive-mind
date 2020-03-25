edeb08fdd0a12e4b95c0e89be0e700cb3c107291
Bobby Nixon <rhanixon1@gmail.com>
2020-03-25 11:58:15 -0700

    added logout button

--- client/components/FBLogin.js
+++ client/components/FBLogin.js
@@ -1,5 +1,5 @@
-import React, { Component, useState } from 'react';
-import { Text, TouchableOpacity, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, View, Image, StyleSheet, ActivityIndicator } from 'react-native';
 // import Expo  from 'expo';
 import * as Facebook from 'expo-facebook';
 
@@ -7,16 +7,10 @@ export default function FBLogin(){
 
 const [isLoggedin, setLoggedinStatus] = useState(false);
 const [userData, setUserData] = useState([]);
-//   callGraph = async token => {
-//     /// Look at the fields... I don't have an `about` on my profile but everything else should get returned.
-//     const response = await fetch(
-//       `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,about,picture`
-//     );
-//     const userData = JSON.stringify(await response.json());
-//     setUserData({ responseJSON });
-//   };
+const [isImageLoading, setImageLoadStatus] = useState(false);
 
-  const login = async () => {
+
+  const facebookLogIn = async () => {
     try {
     await Facebook.initializeAsync('3845977412082426');
     const {type, token } = await Facebook.logInWithReadPermissionsAsync({permissions: ['public_profile', 'email', 'user_friends'],
@@ -41,49 +35,73 @@ const [userData, setUserData] = useState([]);
 const logout = () => {
     setLoggedinStatus(false);
     setUserData(null);
-  }
+    setImageLoadStatus(false);
+  };
 
-  const renderButton = () => (
-    <TouchableOpacity onPress={() => login()}>
-      <View
-        style={{
-          width: '100%',
-          alignSelf: 'center',
-          borderRadius: 4,
-          padding: 24,
-          backgroundColor: '#3B5998',
-        }}>
-        <Text style={{ color: 'white', fontWeight: 'bold' }}>
-          Login with Facebook
-        </Text>
-      </View>
-    </TouchableOpacity>
-  );
-
-    const  renderValue = value => (
-    <Text key={value} >{value}</Text>
-  );
+//   const renderButton = () => (
+//     <TouchableOpacity onPress={() => login()}>
+//       <View
+//         style={{
+//           width: '100%',
+//           alignSelf: 'center',
+//           borderRadius: 4,
+//           padding: 24,
+//           backgroundColor: '#3B5998',
+//         }}>
+//         <Text style={{ color: 'white', fontWeight: 'bold' }}>
+//           Login with Facebook
+//         </Text>
+//       </View>
+//     </TouchableOpacity>
+//   );
 
     return (
     isLoggedin ?
         userData ?
       <View >
-       <Image
+        <ActivityIndicator size="large" color="#0000ff" animating={!isImageLoading} style={{ position: "absolute" }} />
+           <Image
             style={{ width: 200, height: 200, borderRadius: 50 }}
             source={{ uri: userData.picture.data.url }}
             onLoadEnd={() => setImageLoadStatus(true)} />
-    <Text style={{ fontSize: 22, marginVertical: 10, alignContent: "center"}}>Hi {userData.name}!</Text>
-
+    <Text style={{ fontSize: 22, marginVertical: 10}}> Hi {userData.name}!</Text>
+    <TouchableOpacity style={styles.logoutBtn} onPress={logout}>
+            <Text style={{ color: "#fff" }}>Logout</Text>
+          </TouchableOpacity>
 
       </View>
       :
       null
       :
-      <View >
-      {renderButton()}
-    </View>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.loginBtn} onPress={() => facebookLogIn()}>
+          <Text style={{ color: "#fff" }}>Login with Facebook</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 
 
-
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#e9ebee',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    loginBtn: {
+      backgroundColor: '#4267b2',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 20
+    },
+    logoutBtn: {
+      backgroundColor: '#4267b2',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 20,
+      position: "absolute",
+      left: 35,
+      bottom: -50
+    },
+  });
