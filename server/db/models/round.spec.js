1d1abd59c6bb6997e0bbd3d0ba65d848bfbcb3ff
Natasha Whitney <natiwhitney@gmail.com>
2020-03-26 14:42:41 -0400

    Remove gameDate

--- server/db/models/round.spec.js
+++ server/db/models/round.spec.js
@@ -1,19 +1,17 @@
-const { expect } = require("chai");
-const { db, Round } = require(".");
+const { expect } = require('chai');
+const { db, Round } = require('.');
 
-describe("Round model", () => {
+describe('Round model', () => {
   beforeEach(() => db.sync({ force: true }));
-  describe("Column definitions and validations", () => {
-    it("has a gameId, letters, coreLetter, and gameDate property", () => {
+  describe('Column definitions and validations', () => {
+    it('has a gameId, letters, coreLetter, and gameDate property', () => {
       const date = new Date();
       return Round.create({
-        letters: "CDEFGBA",
-        coreLetter: "A",
-        gameDate: date
+        letters: 'CDEFGBA',
+        coreLetter: 'A'
       }).then(round => {
-        expect(round.letters).to.equal("ABCDEFG");
-        expect(round.coreLetter).to.equal("A");
-        expect(round.gameDate).to.deep.equal(date);
+        expect(round.letters).to.equal('ABCDEFG');
+        expect(round.coreLetter).to.equal('A');
       });
     });
   });
