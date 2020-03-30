d6262b82c893073a91b13cfd2d2de796ebbc57b1
Bobby Nixon <rhanixon1@gmail.com>
2020-03-30 16:56:32 -0700

    added thoughts on 1v1 flow and some more logic for room assignment

--- client/screens/LobbyScreen.js
+++ client/screens/LobbyScreen.js
@@ -11,7 +11,15 @@ const LobbyScreen = ({ navigation, user }) => {
   const [usersWaiting, setUsersWaiting] = useState([]);
 
   const WAITING_ROOM = 'waiting_room';
+  const V1_ROOM = curr1v1Room;
 
+  /*
+  Notes: I think that there should be two modes,
+
+  Random Oponent: You enter the lobby and see the waiting screen if no one is in there. Otherwise, you are automatically assigned to a room once another user joins the lobby.
+
+  Play with Friends: You click a diferent button and you are navigated to a screen where you create a room and then invite your friend to join that room. Maybe you can click on your friend from a list of your friends and have this happen dynamically but for now lets just have one user create a room and invite there friend to it.
+  */
   useEffect(() => {
     if (inRoom) {
       socket.emit('join room', {
@@ -20,6 +28,16 @@ const LobbyScreen = ({ navigation, user }) => {
       }); //A room always will be unique, when you do socket.join('roomname') if the room not exist it will created and this socket will join it, if exist the socket just will join it.
 
       setUsersWaiting([...usersWaiting, user]);
+    } else if (inRoom && usersWaiting.length > 1) {
+      socket.emit('join room', {
+        user: user,
+        room: V1_ROOM
+      });
+      socket.clients(curr1v1Room) === 2 //not sure this works based on my web search it seems like the command to get the number of people in a room keeps changing with version updates
+        ? set1v1Room(curr1v1Room + 1)
+        : set1v1Room(curr1v1Room); //increment the room id once 2 users are in it so the next user can joing a unique room
+      setUsersWaiting(usersWaiting.splice(usersWaiting.indexOf(user), 1)); //Remove user from Waiting_Room
+      setInRoom(false);
     }
 
     // This just runs when the component dismounts
@@ -34,26 +52,26 @@ const LobbyScreen = ({ navigation, user }) => {
   });
 
   const handleLeaving = () => {
+    setInRoom(false);
+    setUsersWaiting(usersWaiting.splice(usersWaiting.indexOf(user), 1));
     navigation.navigate('PlayScreen');
   };
 
-  return usersWaiting.length >= 1 ? (
+  return usersWaiting.length > 1 ? (
     <Container>
       <H1 style={styles.lobby}></H1>
       <H3>You have opponents!</H3>
-      <List>
-        {usersWaiting.map(user => (
-          <ListItem key={user.id} title={user.username} bottomDivider>
-            {/* <Text>{l.username}</Text> */}
-          </ListItem>
-        ))}
-      </List>
+
+      {usersWaiting.map(user => (
+        <Text key={user.id}>{user.username}</Text>
+      ))}
     </Container>
   ) : (
     inRoom && (
       <Container>
         <H1 style={styles.lobby}></H1>
         <H3>Waiting for opponent...</H3>
+
         <Button
           primary
           block
@@ -62,7 +80,7 @@ const LobbyScreen = ({ navigation, user }) => {
           title="Leave Lobby"
           onPress={() => handleLeaving()}
         >
-          <Text>Find An Opponent</Text>
+          <Text>Leave Lobby</Text>
         </Button>
       </Container>
     )
--- client/socket.js
+++ client/socket.js
@@ -2,7 +2,8 @@ import io from 'socket.io-client';
 
 // To do: make sure the PORT is correct
 // This will probably work locally and then we have to figure out how to make it work when we deploy
-const PORT = 8080;
+// const PORT = 8080;
+const PORT = 1234;
 const socket = io(`http://localhost:${PORT}`);
 
 // Not sure if we're going to use this, but we'll find out
