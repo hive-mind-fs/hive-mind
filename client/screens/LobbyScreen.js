49aae1383c420e6c170c2a6bf6246cd34155646d
Bobby Nixon <rhanixon1@gmail.com>
2020-04-01 08:55:05 -0700

    play button takes users to countdown and then gb screen

--- client/screens/LobbyScreen.js
+++ client/screens/LobbyScreen.js
@@ -2,10 +2,11 @@ import React, { Fragment, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { StyleSheet } from 'react-native';
 import { Container, H1, H3, Text, Button, List, ListItem } from 'native-base';
+import { fetchPracticeRound } from '../store/game';
 import socket from '../socket';
 
-const LobbyScreen = ({ navigation, user }) => {
-  const [enteredRoom, setEnteredRoom] = useState(true)
+const LobbyScreen = ({ navigation, createUserRound, user }) => {
+  const [enteredRoom, setEnteredRoom] = useState(true);
   const [users, setUsers] = useState([]);
 
   /*
@@ -17,23 +18,26 @@ const LobbyScreen = ({ navigation, user }) => {
   */
   useEffect(() => {
     if (enteredRoom) {
-
-    socket.emit('join room', {
+      socket.emit('join room', {
         user: user
       });
 
-    setEnteredRoom(false)
+      setEnteredRoom(false);
     }
   }); //only run once ...
 
   useEffect(() => {
     socket.on('game ready!', data => {
-      const gameData = JSON.parse(data)
-      console.log(`Me ${user.username} has data for game`, gameData)
-      setUsers(gameData.users)
-    })
-  }, [users])
+      const gameData = JSON.parse(data);
+      console.log(`Me ${user.username} has data for game`, gameData);
+      setUsers(gameData.users);
+    });
+  }, [users]);
 
+  const handlePlay = () => {
+    createUserRound(user.id);
+    navigation.navigate('CountdownScreen');
+  };
 
   const handleLeaving = () => {
     // Given game ready
@@ -48,30 +52,52 @@ const LobbyScreen = ({ navigation, user }) => {
       <H1 style={styles.lobby}></H1>
       <H3>You have opponents!</H3>
       <List>
-        {users.map((user,i) => (
+        {users.map((user, i) => (
           <ListItem key={i} bottomDivider>
             <Text>{user.username}</Text>
           </ListItem>
         ))}
       </List>
+
+      <Button
+        primary
+        block
+        rounded
+        marginTopL
+        title="Practice"
+        onPress={() => handlePlay()}
+      >
+        <Text>Play</Text>
+      </Button>
+
+      <Button
+        primary
+        block
+        rounded
+        marginTopL
+        title="Leave"
+        onPress={() => handleLeaving()}
+      >
+        <Text>Leave</Text>
+      </Button>
     </Container>
   ) : (
-      <Container>
-        <H1 style={styles.lobby}></H1>
-        <H3>Waiting for opponent...</H3>
+    <Container>
+      <H1 style={styles.lobby}></H1>
+      <H3>Waiting for opponent...</H3>
 
-        <Button
-          primary
-          block
-          rounded
-          marginTopL
-          title="Leave Lobby"
-          onPress={() => handleLeaving()}
-        >
-          <Text>Leave Lobby</Text>
-        </Button>
-      </Container>
-    )
+      <Button
+        primary
+        block
+        rounded
+        marginTopL
+        title="Leave Lobby"
+        onPress={() => handleLeaving()}
+      >
+        <Text>Leave Lobby</Text>
+      </Button>
+    </Container>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -87,4 +113,10 @@ const mapState = state => {
   };
 };
 
-export default connect(mapState)(LobbyScreen);
+const mapDispatch = dispatch => {
+  return {
+    createUserRound: userId => dispatch(fetchPracticeRound(userId))
+  };
+};
+
+export default connect(mapState, mapDispatch)(LobbyScreen);
