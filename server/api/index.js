0442b4b16eea9995f980983dc9355ad796f758a3
richmcateer <rmcateer1@gmail.com>
2020-04-03 15:52:33 -0400

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,6 @@ module.exports = router;
 
 // router.use("/users", require("./users"));
 // router.use("/games", require("./games"));
-router.use('/rounds', require('./rounds'));
 router.use('/userRounds', require('./userRounds'));
 // router.use("/words", require("./words"));
 
