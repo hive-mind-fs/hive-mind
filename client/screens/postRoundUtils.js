712d319ac6d27525e63e5d1bf096c9b207fbccb3
Natasha Whitney <natiwhitney@gmail.com>
2020-04-04 19:13:59 -0400

    Fix util function

--- client/screens/postRoundUtils.js
+++ client/screens/postRoundUtils.js
@@ -2,7 +2,7 @@ const sumWordLength = (acc, curr ) => acc + curr.word.length
 
 export const getAverageWordLength = words => words.length ? words.reduce(sumWordLength,0)/words.length : 0
 
-export const getPangrams = userWords = userWords.filter(word => {
+export const getPangrams = userWords => userWords.filter(word => {
           const uniqueLetters = new Set(...word.word.split(''));
           return uniqueLetters === 7;
         });
