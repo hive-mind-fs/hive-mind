c3e39495a73cd58d012c6255050961dbc2150e8e
Natasha Whitney <natiwhitney@gmail.com>
2020-04-04 19:31:55 -0400

    Remove PostRound1v1Screen button on PlayScreen

--- client/screens/PlayScreen.js
+++ client/screens/PlayScreen.js
@@ -67,16 +67,6 @@ function PlayScreen({ navigation, createUserRound, user }) {
       >
         <Text>How To Play</Text>
       </Button>
-
-      <Button
-        light
-        block
-        rounded
-        marginTop
-        onPress={() => navigation.navigate('PostRound1v1Screen')}
-      >
-        <Text>Post1v1</Text>
-      </Button>
     </Container>
   );
 }
