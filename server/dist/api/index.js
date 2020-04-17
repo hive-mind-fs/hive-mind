d11a17085ada56570ecbfde3ab085deb39e35b93
Rich McAteer <rmcateer1@gmail.com>
2020-04-17 13:16:05 -0400

    Remove dist

--- server/dist/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-module.exports = router;
-router.use('/userRounds', require('./userRounds'));
-router.use((req, res, next) => {
-    const error = new Error('Not Found');
-    next(error);
-});
-module.exports = router;
--- server/dist/index.js
+++
@@ -1,4 +0,0 @@
-console.log('Blah!');
-console.log('TEST!');
-console.log('3');
-console.log('4');
